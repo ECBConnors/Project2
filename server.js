79a1f7ac8d51bca2ef4b14a4d5c9cafb0baa9480
Evan Connors <ecbconnors@gmail.com>
2018-07-06 11:52:11 -0400

    add mongo connection

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+const mongouri = process.env.MONGODB_URI || 'mongodb://localhost:27017/test_app'
 
 app.get('/', (req, res) => {
   res.send('testing');
+});
+
+mongoose.connect(mongouri);
+mongoose.connection.on('open', () => {
+  console.log('############connected###########');
 });
 
 app.listen(port, () => {
