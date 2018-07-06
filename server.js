4a918bfdadcf428260ee3ed26a618c6feb4b463c
Evan Connors <ecbconnors@gmail.com>
2018-07-06 14:46:06 -0400

    skeleton filesystem should be set up now

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
-const mongouri = process.env.MONGODB_URI || 'mongodb://localhost:27017/test_app'
+const mongouri = process.env.MONGODB_URI || 'mongodb://localhost:27017/test_app';
 
 app.get('/', (req, res) => {
   res.send('testing');
